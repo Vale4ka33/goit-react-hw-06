c5b9be268b6ceb3cc72b71a7abe420ffdedcc548
Valentina Ukho <petrova.hfhfhf@gmail.com>
2024-06-18 13:38:17 +0300

    fix filter contact and App

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,22 +2,27 @@ import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 import { selectContacts } from "../../redux/contactsSlice";
+import { selectNameFilter } from "../../redux/filtersSlice";
 
 const ContactList = () => {
   const contacts = useSelector(selectContacts);
+  const filter = useSelector(selectNameFilter);
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <ul className={css.contactList}>
-      {contacts.map((contactItem) => {
-        return (
-          <li key={contactItem.id} className={css.contactItems}>
-            <Contact
-              name={contactItem.name}
-              number={contactItem.number}
-              id={contactItem.id}
-            />
-          </li>
-        );
-      })}
+      {filteredContacts.map((contactItem) => (
+        <li key={contactItem.id} className={css.contactItems}>
+          <Contact
+            name={contactItem.name}
+            number={contactItem.number}
+            id={contactItem.id}
+          />
+        </li>
+      ))}
     </ul>
   );
 };
